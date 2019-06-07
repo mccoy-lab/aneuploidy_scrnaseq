39812a80195bcc904830dff08753d4d6552caa89
Rajiv McCoy <rajiv.mccoy@jhu.edu>
2019-06-07 13:28:17 -0400

    sbatch each cell

--- genotyping/03_allelic_imbalance_embryo.sh
+++ genotyping/03_allelic_imbalance_embryo.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 #SBATCH --partition=shared
-#SBATCH --job-name=allelic_imbalance
+#SBATCH --job-name=allelic_imbalance_embryo
 #SBATCH --time=3:0:0
 #SBATCH --array=1-88%88
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=8
+#SBATCH --ntasks-per-node=4
 
 ml samtools
 ml htslib
@@ -88,52 +88,7 @@ tabix -p vcf ${embryo}/${embryo}_AF.vcf.gz
 bgzip ${embryo}/${embryo}_knownSNP.vcf
 tabix -p vcf ${embryo}/${embryo}_knownSNP.vcf.gz
 
-N=8
-(
 for cell_accession in ${accession_list}
 do
-  ((i=i%N)); ((i++==0)) && wait
-  (
-  # add read group information
-  picard AddOrReplaceReadGroups \
-    I=${cell_accession}/${cell_accession}Aligned.sortedByCoord.out.bam \
-    O=${cell_accession}/${cell_accession}_reheader.bam \
-    RGLB=${cell_accession} \
-    RGPL=illumina \
-    RGPU=${cell_accession} \
-    RGSM=${cell_accession}
-  
-  # mark duplicates
-  picard MarkDuplicates \
-    I=${cell_accession}/${cell_accession}_reheader.bam \
-    O=${cell_accession}/${cell_accession}_dedupped.bam \
-    CREATE_INDEX=true \
-    VALIDATION_STRINGENCY=SILENT \
-    M=${cell_accession}/${cell_accession}.metrics
-  
-  samtools index ${cell_accession}/${cell_accession}_dedupped.bam
-  
-  # "split'n'trim" the reads and adjust mapping quality
-  java -jar ~/work/progs/GenomeAnalysisTK-3.8-1-0-gf15c1c3ef/GenomeAnalysisTK.jar \
-    -T SplitNCigarReads \
-    -R /work-zfs/rmccoy22/resources/reference/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna \
-    -I ${cell_accession}/${cell_accession}_dedupped.bam \
-    -o ${cell_accession}/${cell_accession}_split.bam \
-    -rf ReassignOneMappingQuality \
-    -RMQF 255 \
-    -RMQT 60 \
-    -U ALLOW_N_CIGAR_READS
-  
-  # then run the ASE read counter on individual cell BAMs at those heterozygous sites
-  # https://software.broadinstitute.org/gatk/documentation/tooldocs/3.8-0/org_broadinstitute_gatk_tools_walkers_rnaseq_ASEReadCounter.php
-  ~/work/progs/gatk-4.0.12.0/gatk \
-    ASEReadCounter \
-    -R /work-zfs/rmccoy22/resources/reference/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna \
-    -I ${cell_accession}/${cell_accession}_split.bam \
-    -V ${embryo}/${embryo}_knownSNP.vcf.gz \
-    -O ${cell_accession}/${cell_accession}.table
-  ) &
+  sbatch --export=cell_accession=${cell_accession},embryo=${embryo} 04_allelic_imbalance_cell.sh
 done
-)
-
-wait
