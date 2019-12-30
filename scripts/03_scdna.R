7febd1077d30699fceeb96fdb831fb1ff7ab6bfc
Rajiv McCoy <rajiv.mccoy@jhu.edu>
2019-12-30 16:23:40 -0500

    add session info

--- scripts/03_scdna.R
+++ scripts/03_scdna.R
@@ -158,11 +158,15 @@ embryo_heatmaps[[length(embryo_heatmaps) + 1]] <- legend
 # zhou et al. 2019; scDNA-seq
 
 url <- "https://static-content.springer.com/esm/art%3A10.1038%2Fs41586-019-1500-0/MediaObjects/41586_2019_1500_MOESM3_ESM.zip"
-download.file(url, here("41586_2019_1500_MOESM3_ESM.zip"))
-unzip(here("41586_2019_1500_MOESM3_ESM.zip"), files = "Supplementary Table 1 Single cells and embryos information.xlsx")
+download.file(url, here("external_metadata/41586_2019_1500_MOESM3_ESM.zip"))
+unzip(here("external_metadata/41586_2019_1500_MOESM3_ESM.zip"), 
+      files = "Supplementary Table 1 Single cells and embryos information.xlsx",
+      exdir = here("external_metadata"))
+file.rename(here("external_metadata/Supplementary Table 1 Single cells and embryos information.xlsx"),
+            here("external_metadata/zhou2019_tableS1.xlsx"))
 
 # to read from URL, uncomment lines below
-dt_zhou <- read_xlsx(here("Supplementary Table 1 Single cells and embryos information.xlsx"), sheet = 2, skip = 2, na = "") %>%
+dt_zhou <- read_xlsx(here("external_metadata/zhou2019_tableS1.xlsx"), sheet = 2, skip = 2, na = "") %>%
   as.data.table()
 dt_zhou <- dt_zhou[, c(2:4, 11:16)]
 dt_zhou <- dt_zhou[!is.na(Sample) & Sample != "Total"] %>%
@@ -187,9 +191,13 @@ summary(m3)
 
 # zhou et al. 2019; scRNA-seq
 
-unzip(here("41586_2019_1500_MOESM3_ESM.zip"), files = "Supplementary Table 2 Sample Information.xlsx")
+unzip(here("external_metadata/41586_2019_1500_MOESM3_ESM.zip"), 
+      files = "Supplementary Table 2 Sample Information.xlsx",
+      exdir = here("external_metadata"))
+file.rename(here("external_metadata/Supplementary Table 2 Sample Information.xlsx"),
+            here("external_metadata/zhou2019_tableS2.xlsx"))
 
-dt_zhou_rna <- read_xlsx(here("Supplementary Table 2 Sample Information.xlsx"), sheet = 1, na = "") %>%
+dt_zhou_rna <- read_xlsx(here("external_metadata/zhou2019_tableS2.xlsx"), sheet = 1, na = "") %>%
   as.data.table()
 
 dt_zhou_rna[, is_aneuploid := (CNV == "Abnormal")]
@@ -207,4 +215,107 @@ summary(margins(m4))
 
 m5 <- glmer(data = dt_zhou_rna, formula = is_aneuploid ~ (1 | Ori_Day_Emb) + num_estage * is_trophectoderm, family = "binomial")
 summary(m5)
+
+###
+
+devtools::session_info()
+
+# ─ Session info ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+# setting  value                       
+# version  R version 3.6.1 (2019-07-05)
+# os       macOS Mojave 10.14.6        
+# system   x86_64, darwin15.6.0        
+# ui       RStudio                     
+# language (EN)                        
+# collate  en_US.UTF-8                 
+# ctype    en_US.UTF-8                 
+# tz       America/New_York            
+# date     2019-12-30                  
+# 
+# ─ Packages ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+# package     * version  date       lib source        
+# assertthat    0.2.1    2019-03-21 [1] CRAN (R 3.6.0)
+# backports     1.1.5    2019-10-02 [1] CRAN (R 3.6.0)
+# boot          1.3-23   2019-07-05 [1] CRAN (R 3.6.0)
+# broom         0.5.2    2019-04-07 [1] CRAN (R 3.6.0)
+# callr         3.3.2    2019-09-22 [1] CRAN (R 3.6.0)
+# cellranger    1.1.0    2016-07-27 [1] CRAN (R 3.6.0)
+# cli           1.1.0    2019-03-19 [1] CRAN (R 3.6.0)
+# colorspace    1.4-1    2019-03-18 [1] CRAN (R 3.6.0)
+# cowplot     * 1.0.0    2019-07-11 [1] CRAN (R 3.6.0)
+# crayon        1.3.4    2017-09-16 [1] CRAN (R 3.6.0)
+# curl          4.2      2019-09-24 [1] CRAN (R 3.6.0)
+# data.table  * 1.12.6   2019-10-18 [1] CRAN (R 3.6.0)
+# DBI           1.0.0    2018-05-02 [1] CRAN (R 3.6.0)
+# dbplyr        1.4.2    2019-06-17 [1] CRAN (R 3.6.0)
+# desc          1.2.0    2018-05-01 [1] CRAN (R 3.6.0)
+# devtools      2.2.1    2019-09-24 [1] CRAN (R 3.6.0)
+# digest        0.6.23   2019-11-23 [1] CRAN (R 3.6.0)
+# dplyr       * 0.8.3    2019-07-04 [1] CRAN (R 3.6.0)
+# ellipsis      0.3.0    2019-09-20 [1] CRAN (R 3.6.0)
+# farver        2.0.1    2019-11-13 [1] CRAN (R 3.6.0)
+# forcats     * 0.4.0    2019-02-17 [1] CRAN (R 3.6.0)
+# fs            1.3.1    2019-05-06 [1] CRAN (R 3.6.0)
+# generics      0.0.2    2018-11-29 [1] CRAN (R 3.6.0)
+# ggdendro    * 0.1-20   2016-04-27 [1] CRAN (R 3.6.0)
+# ggplot2     * 3.2.1    2019-08-10 [1] CRAN (R 3.6.0)
+# glue          1.3.1    2019-03-12 [1] CRAN (R 3.6.0)
+# gtable        0.3.0    2019-03-25 [1] CRAN (R 3.6.0)
+# haven         2.2.0    2019-11-08 [1] CRAN (R 3.6.0)
+# here        * 0.1      2017-05-28 [1] CRAN (R 3.6.0)
+# hms           0.5.2    2019-10-30 [1] CRAN (R 3.6.0)
+# httr        * 1.4.1    2019-08-05 [1] CRAN (R 3.6.0)
+# jsonlite      1.6      2018-12-07 [1] CRAN (R 3.6.0)
+# labeling      0.3      2014-08-23 [1] CRAN (R 3.6.0)
+# lattice       0.20-38  2018-11-04 [1] CRAN (R 3.6.1)
+# lazyeval      0.2.2    2019-03-15 [1] CRAN (R 3.6.0)
+# lifecycle     0.1.0    2019-08-01 [1] CRAN (R 3.6.0)
+# lme4        * 1.1-21   2019-03-05 [1] CRAN (R 3.6.0)
+# lubridate     1.7.4    2018-04-11 [1] CRAN (R 3.6.0)
+# magrittr      1.5      2014-11-22 [1] CRAN (R 3.6.0)
+# margins     * 0.3.23   2018-05-22 [1] CRAN (R 3.6.0)
+# MASS          7.3-51.4 2019-03-31 [1] CRAN (R 3.6.1)
+# Matrix      * 1.2-17   2019-03-22 [1] CRAN (R 3.6.1)
+# memoise       1.1.0    2017-04-21 [1] CRAN (R 3.6.0)
+# minqa         1.2.4    2014-10-09 [1] CRAN (R 3.6.0)
+# modelr        0.1.5    2019-08-08 [1] CRAN (R 3.6.0)
+# munsell       0.5.0    2018-06-12 [1] CRAN (R 3.6.0)
+# nlme          3.1-142  2019-11-07 [1] CRAN (R 3.6.0)
+# nloptr        1.2.1    2018-10-03 [1] CRAN (R 3.6.0)
+# pillar        1.4.2    2019-06-29 [1] CRAN (R 3.6.0)
+# pkgbuild      1.0.6    2019-10-09 [1] CRAN (R 3.6.0)
+# pkgconfig     2.0.3    2019-09-22 [1] CRAN (R 3.6.0)
+# pkgload       1.0.2    2018-10-29 [1] CRAN (R 3.6.0)
+# prediction    0.3.14   2019-06-17 [1] CRAN (R 3.6.0)
+# prettyunits   1.0.2    2015-07-13 [1] CRAN (R 3.6.0)
+# processx      3.4.1    2019-07-18 [1] CRAN (R 3.6.0)
+# ps            1.3.0    2018-12-21 [1] CRAN (R 3.6.0)
+# purrr       * 0.3.3    2019-10-18 [1] CRAN (R 3.6.0)
+# R6            2.4.1    2019-11-12 [1] CRAN (R 3.6.0)
+# Rcpp          1.0.3    2019-11-08 [1] CRAN (R 3.6.0)
+# readr       * 1.3.1    2018-12-21 [1] CRAN (R 3.6.0)
+# readxl      * 1.3.1    2019-03-13 [1] CRAN (R 3.6.0)
+# remotes       2.1.0    2019-06-24 [1] CRAN (R 3.6.0)
+# reprex        0.3.0    2019-05-16 [1] CRAN (R 3.6.0)
+# rlang         0.4.2    2019-11-23 [1] CRAN (R 3.6.0)
+# rprojroot     1.3-2    2018-01-03 [1] CRAN (R 3.6.0)
+# rstudioapi    0.10     2019-03-19 [1] CRAN (R 3.6.0)
+# rvest         0.3.5    2019-11-08 [1] CRAN (R 3.6.0)
+# scales        1.1.0    2019-11-18 [1] CRAN (R 3.6.0)
+# sessioninfo   1.1.1    2018-11-05 [1] CRAN (R 3.6.0)
+# stringi       1.4.3    2019-03-12 [1] CRAN (R 3.6.0)
+# stringr     * 1.4.0    2019-02-10 [1] CRAN (R 3.6.0)
+# testthat      2.3.0    2019-11-05 [1] CRAN (R 3.6.0)
+# tibble      * 2.1.3    2019-06-06 [1] CRAN (R 3.6.0)
+# tidyr       * 1.0.0    2019-09-11 [1] CRAN (R 3.6.0)
+# tidyselect    0.2.5    2018-10-11 [1] CRAN (R 3.6.0)
+# tidyverse   * 1.3.0    2019-11-21 [1] CRAN (R 3.6.0)
+# usethis       1.5.1    2019-07-04 [1] CRAN (R 3.6.0)
+# vctrs         0.2.0    2019-07-05 [1] CRAN (R 3.6.0)
+# withr         2.1.2    2018-03-15 [1] CRAN (R 3.6.0)
+# xml2          1.2.2    2019-08-09 [1] CRAN (R 3.6.0)
+# yaml          2.2.0    2018-07-25 [1] CRAN (R 3.6.0)
+# zeallot       0.1.0    2018-01-28 [1] CRAN (R 3.6.0)
+# 
+# [1] /Library/Frameworks/R.framework/Versions/3.6/Resources/library
 
